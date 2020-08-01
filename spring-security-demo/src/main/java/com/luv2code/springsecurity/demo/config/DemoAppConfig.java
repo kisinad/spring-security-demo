227791394b1d3a52f8da859302f20c4230d6d169
Denis Kisina <kisinad@gmail.com>
2020-08-01 12:18:17 -0500

    added the spring security dependences (spring.security.web & spring.secuirty.config)

--- spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,5 +1,6 @@
 package com.luv2code.springsecurity.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
 public class DemoAppConfig {
 
+	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
---
+++ spring-security-demo/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Luv2Code Home page</title>
+</head>
+<body>
+	<h2>Luv2Code company Home page</h2>
+	Welcome to the luv2code company Home Page
+</body>
+</html>
